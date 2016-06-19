1530e1a738fbe4fdd6c324a8a884bc09e32f78a4
kbmms <roxsublime@gmail.com>
2016-06-19 00:45:22 -0300

    iniciando com angular em portfolio 1.1

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,8 @@
 var app = angular.module("meuPortfolio",[]);
 
-app.controller("ctrl", function(){
-  
+app.controller("ctrl", function($scope, $http){
+  $http.get("javascripts/portfolio.json").success(function(response){
+    $scope.sites = response;
+    console.log(response);
+  })
 })
