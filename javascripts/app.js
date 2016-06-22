98dd200ba944a12a5bd1217e2aeeefb221e0753d
kbmms <roxsublime@gmail.com>
2016-06-22 19:17:36 -0300

    testes em bg portfolio 1.2

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,4 @@
 var app = angular.module("meuPortfolio",[]);
-
 app.controller("ctrl", function($scope, $http){
   $http.get("javascripts/portfolio.json").success(function(response){
     $scope.sites = response.sites;
