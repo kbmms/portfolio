4ce5a42e6659380f66fe8da38ba556b59aac851d
Marcel <roxsublime@gmail.com>
2023-09-11 10:31:00 -0300

    new portfolio

--- javascripts/app.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module("meuPortfolio",[]);
-app.controller("ctrl", function($scope, $http){
-  $http.get("javascripts/portfolio.json").success(function(response){
-    $scope.sites = response.sites;
-    console.log(response);
-  })
-})
